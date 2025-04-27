02404003d284e9caf5bb4bc1b1a066884dbbb979
Isaac Hatilima <isaachatilima@gmail.com>
2025-04-27 15:01:31 +0200

    Refactored side nav

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,21 +1,36 @@
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
+import { NavGroup } from '@/types';
 import { Link } from '@inertiajs/react';
 import { LayoutGrid, ListTodo } from 'lucide-react';
 import AppLogo from './app-logo';
 
-const mainNavItems: NavItem[] = [
+const mainNavItems: NavGroup[] = [
     {
         title: 'Dashboard',
-        href: route('dashboard'),
-        icon: LayoutGrid,
+        items: [
+            {
+                title: 'Dashboard',
+                href: route('dashboard'),
+                icon: LayoutGrid,
+            },
+        ],
     },
     {
         title: 'Todos',
-        href: route('todos.index'),
-        icon: ListTodo,
+        items: [
+            {
+                title: 'My Todos',
+                href: route('todos.index'),
+                icon: ListTodo,
+            },
+            {
+                title: 'Invited Todos',
+                href: route('todos.index'),
+                icon: ListTodo,
+            },
+        ],
     },
 ];
 
--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,24 +1,33 @@
 import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
-import { type NavItem } from '@/types';
+import { NavGroup } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 
-export function NavMain({ items = [] }: { items: NavItem[] }) {
+export function NavMain({ items = [] }: { items: NavGroup[] }) {
     const page = usePage();
     return (
-        <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Menu</SidebarGroupLabel>
-            <SidebarMenu>
-                {items.map((item) => (
-                    <SidebarMenuItem key={item.title}>
-                        <SidebarMenuButton asChild isActive={item.href === page.url} tooltip={{ children: item.title }}>
-                            <Link href={item.href} prefetch>
-                                {item.icon && <item.icon />}
-                                <span>{item.title}</span>
-                            </Link>
-                        </SidebarMenuButton>
-                    </SidebarMenuItem>
-                ))}
-            </SidebarMenu>
-        </SidebarGroup>
+        <>
+            {items.map((group) => (
+                <SidebarGroup key={group.title} className="px-2 py-0">
+                    <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+
+                    <SidebarMenu>
+                        {group.items.map((item) => {
+                            const itemPath = new URL(item.href, window.location.origin).pathname;
+
+                            return (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild isActive={page.url.startsWith(itemPath)} tooltip={{ children: item.title }}>
+                                        <Link href={item.href} prefetch>
+                                            {item.icon && <item.icon />}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            );
+                        })}
+                    </SidebarMenu>
+                </SidebarGroup>
+            ))}
+        </>
     );
 }
