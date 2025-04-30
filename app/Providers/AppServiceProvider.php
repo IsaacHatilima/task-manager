19bcacb11608694dc11b3d1c9982e3a8ec3e04b8
Isaac Hatilima <isaachatilima@gmail.com>
2025-04-30 10:58:13 +0200

    Todo observer added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Todo;
+use App\Observers\TodoObserver;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -23,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Model::automaticallyEagerLoadRelationships();
         Schema::defaultStringLength(191);
+
+        Todo::observe(TodoObserver::class);
     }
 }
